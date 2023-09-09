27fee8183cbf5e5974e8dc4955cc34ab56c76a8a
wam24 <wmontero@zxxztech.com>
2023-09-09 16:02:27 -0400

    [UPDATE] + v2 del reto

--- www/bank/admin.py
+++ www/bank/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Usuario
+
+
 # Register your models here.
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('rut',)
+    list_display = ("id", "rut",)
+    ordering = ("id",)
+    list_display_links = ("id", "rut")
--- www/bank/models.py
+++ www/bank/models.py
@@ -12,13 +12,15 @@ STATUS_USUARIO = (
 class Usuario(AbstractUser):
     rut = models.CharField(max_length=8, null=True, unique=True, verbose_name='Rut')
     account_number = models.PositiveIntegerField(null=True, verbose_name='Numero de cuenta')
-    countable_balance = models.DecimalField( null=True, decimal_places=2, max_digits=10)
-    balance_available_current_account = models.DecimalField(null=True, decimal_places=2, max_digits=10)
-    available_credit_line_balance = models.DecimalField(null=True, decimal_places=2, max_digits=10)
-    total_charges = models.DecimalField( null=True, decimal_places=2, max_digits=10)
-    status = models.CharField(choices=STATUS_USUARIO, default='ACTIVE', max_length=64)
-    failed_login_attempts = models.PositiveIntegerField(default=0,)
-    total_abonos = models.DecimalField(null=True, decimal_places=2, max_digits=10) # No se consiguio traduccion literal
+    countable_balance = models.DecimalField(null=True, decimal_places=2, max_digits=10, verbose_name='Saldo contable')
+    balance_available_current_account = models.DecimalField(null=True, decimal_places=2, max_digits=10,
+                                                            verbose_name='Saldo disponible cuenta corriente')
+    available_credit_line_balance = models.DecimalField(null=True, decimal_places=2, max_digits=10,
+                                                        verbose_name='Saldo disponible línea de credito')
+    total_charges = models.DecimalField(null=True, decimal_places=2, max_digits=10, verbose_name='Total cargos')
+    status = models.CharField(choices=STATUS_USUARIO, default='ACTIVE', max_length=64, verbose_name='Estado')
+    failed_login_attempts = models.PositiveIntegerField(default=0, verbose_name='Intentos fallidos al iniciar sesión')
+    total_abonos = models.DecimalField(null=True, decimal_places=2, max_digits=10, verbose_name='Total abonos')  # No se consiguio traduccion literal
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
